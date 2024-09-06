8d3d58236c7e63b92fce87bd17c899a7f4459c6d
0knoSakr <sakura.okano@gmail.com>
2024-09-06 17:12:43 +0900

    ListPage.jsに新規登録ボタンを追加

--- customer-management-system/client/src/pages/ListPage.js
+++ customer-management-system/client/src/pages/ListPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link, Outlet, useNavigate } from "react-router-dom";
+
 
 const ListPage = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>顧客一覧ページ</h1>
       {/* ここに顧客一覧を表示するコンポーネントを追加 */}
+      <button onClick={() => navigate("/new")}>新規登録</button>
     </div>
   );
 };
