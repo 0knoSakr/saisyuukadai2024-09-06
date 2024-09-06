7ee7fa7fb665f6d2c1aadf3779fb288b1f3ca992
0knoSakr <sakura.okano@gmail.com>
2024-09-06 11:32:43 +0900

    NewCustomerPage.jsリストを追加

---
+++ reactStudy-main/client/src/pages/NewCustomerPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NewCustomerPage = () => {
+  return (
+    <div>
+      <h1>新規顧客登録ページ</h1>
+      <ul>
+        <li>アポ日付</li>
+        <li>契約した売上</li>
+        <li>会社名</li>
+        <li>会社名かな</li>
+        <li>担当者名</li>
+        <li>資本金</li>
+        <li>従業員数</li>
+        <li>URL</li>
+        <li>現在契約本数</li>
+        <li>アポ先部署</li>
+        <li>会社の所在地</li>
+        <li>アポ内容</li>
+        <li>目標数値</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NewCustomerPage;
