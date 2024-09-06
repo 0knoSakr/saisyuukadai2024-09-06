89d067772eaea5240e2524c4463ee7fa880619d2
0knoSakr <sakura.okano@gmail.com>
2024-09-06 11:25:39 +0900

    ListPage.jsファイルの追加

---
+++ reactStudy-main/client/src/pages/ListPage.js
@@ -0,0 +1,19 @@
+// /src/pages/ListPage.js
+import React from 'react';
+
+const ListPage = () => {
+  return (
+    <div>
+      <h1>顧客一覧ページ</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default ListPage;
