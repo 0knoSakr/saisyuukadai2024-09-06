e70edb7ff9c82411114e877e8ee977000b84d51e
0knoSakr <sakura.okano@gmail.com>
2024-09-06 11:28:14 +0900

    ListPages.jsリストを追加

--- reactStudy-main/client/src/pages/ListPage.js
+++ reactStudy-main/client/src/pages/ListPage.js
@@ -1,16 +1,21 @@
 // /src/pages/ListPage.js
-import React from 'react';
+import React from "react";
 
 const ListPage = () => {
   return (
     <div>
       <h1>顧客一覧ページ</h1>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>アポ日付</li>
+        <li>契約した売上</li>
+        <li>会社名</li>
+        <li>会社名かな</li>
+        <li>担当者名</li>
+        <li>資本金</li>
+        <li>従業員数</li>
+        <li>URL</li>
+        <li>現在契約本数</li>
+        <li>アポ先部署</li>
       </ul>
     </div>
   );
