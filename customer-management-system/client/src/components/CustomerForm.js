4a44233c18f68091bfd2f6342514a813b05823c1
0knoSakr <sakura.okano@gmail.com>
2024-09-11 14:12:32 +0900

    フォームのコンポーネントを作成

---
+++ customer-management-system/client/src/components/CustomerForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const CustomerForm = ({ formData, handleChange, handleSubmit }) => {
+  return (
+    <form onSunmit={handleSubmit}>
+      <div>
+        <label>会社名:</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      </div>
+      <div>
+        <label>担当者名:</label>
+        <input type="text" name="manager" value={formData.manager} onChange={handleChange} />
+      </div>
+      <div>
+        <label>メールアドレス:</label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </div>
+      <div>
+        <label>電話番号:</label>
+        <input type="tel" name="phone" value={formData.phone} onChange={handleChange} />
+      </div>
+      <div>
+        <button type="submit">送信</button>
+      </div>
+    </form>
+  )
+};
+
+export default CustomerForm;
